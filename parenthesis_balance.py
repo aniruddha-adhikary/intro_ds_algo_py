3094999e2aa91fefa41266e9b331235c7fa9fd22
Aniruddha Adhikary <aniruddha@adhikary.net>
2017-10-29 08:53:29 +0600

    Update parenthesis_balance.py

--- parenthesis_balance.py
+++ parenthesis_balance.py
@@ -1,34 +1,15 @@
-class Stack:
-    def __init__(self):
-        self.items = []
-
-
-    def push(self, item):
-        self.items.append(item)
-
-
-    def pop(self):
-        return self.items.pop()
-
-
-    def is_empty(self):
-        if self.items == []:
-            return True
-        return False
-
-
 def is_balanced(input_str):
-    s = Stack()
+    s = []
     for ch in input_str:
         if ch == '(':
-            s.push(ch)
+            s.append(ch)
         if ch == ')':
-            if s.is_empty():
+            if s:
                 return False
             previous_ch = s.pop()
             if previous_ch != '(':
                 return False
-    if s.is_empty():
+    if not s:
         return True
     return False
 
