8cd32eb3927697a2a2844c7b7a9917e52f9e1656
Aniruddha Adhikary <aniruddha@adhikary.net>
2017-10-29 08:58:47 +0600

    stack inherits directly from list

--- parenthesis_balance.py
+++ parenthesis_balance.py
@@ -1,24 +1,14 @@
-class Stack:
-    def __init__(self):
-        self.items = []
-
-
+class Stack(list):
     def push(self, item):
-        self.items.append(item)
-
-
-    def pop(self):
-        return self.items.pop()
-
+        self.append(item)
 
     def is_empty(self):
-        if self.items == []:
-            return True
-        return False
+        return not self
 
 
 def is_balanced(input_str):
     s = Stack()
+
     for ch in input_str:
         if ch == '(':
             s.push(ch)
@@ -28,13 +18,13 @@ def is_balanced(input_str):
             previous_ch = s.pop()
             if previous_ch != '(':
                 return False
-    if s.is_empty():
-        return True
-    return False
+
+    return s.is_empty()
 
 
 if __name__ == "__main__":
     input_str = input() 
+
     if is_balanced(input_str):    
         print(input_str, "is balanced.")
     else:
